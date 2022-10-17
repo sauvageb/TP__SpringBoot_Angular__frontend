2eda3242186cce4b4089233fcfffd59120b792d9
sauvageb <sauvageboris.pro@gmail.com>
2022-10-17 20:54:02 +0200

    feat(BookService): implement search book by title

--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -38,4 +38,8 @@ export class BookService {
     return this.http.put(`${baseUrl}/${id}`, data);
   }
 
+  findByTitle(title: any): Observable<Book[]> {
+    return this.http.get<Book[]>(`${baseUrl}?title=${title}`);
+  }
+
 }
