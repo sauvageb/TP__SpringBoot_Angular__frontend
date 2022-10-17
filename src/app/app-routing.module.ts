2de62926008efc1490df54ce6adde5a8699ccd61
sauvageb <sauvageboris.pro@gmail.com>
2022-10-17 15:45:38 +0200

    create routing and navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {BookListComponent} from "./components/book-list/book-list.component";
+import {BookDetailsComponent} from "./components/book-details/book-details.component";
+import {AddBookComponent} from "./components/add-book/add-book.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'books', pathMatch: 'full'},
+  {path: 'books', component: BookListComponent},
+  {path: 'books/:id', component: BookDetailsComponent},
+  {path: 'add-book', component: AddBookComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
