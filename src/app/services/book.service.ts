df18ebdef9d2b8043a5815c47a4bd0c802a66ec6
sauvageb <sauvageboris.pro@gmail.com>
2022-10-17 17:54:18 +0200

    create BookService with getAll method

--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -1,9 +1,21 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {Book} from "../models/book";
+
+const baseUrl = 'http://localhost:8080/api/books';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  getAll(): Observable<Book[]> {
+    return this.http.get<Book[]>(baseUrl);
+  }
+
+
 }
