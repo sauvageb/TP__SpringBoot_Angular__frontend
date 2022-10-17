57aaac763ade4babd88ca4ca7650cf8ff37f99a4
sauvageb <sauvageboris.pro@gmail.com>
2022-10-17 18:57:09 +0200

    feat(AddBook): implement add book form to create book with REST API

--- src/app/components/add-book/add-book.component.ts
+++ src/app/components/add-book/add-book.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Book} from "../../models/book";
+import {BookService} from "../../services/book.service";
+import {DatePipe} from "@angular/common";
 
 @Component({
   selector: 'app-add-book',
@@ -7,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddBookComponent implements OnInit {
 
-  constructor() { }
+  book: Book = {
+    title: '',
+    description: '',
+    pictureUrl: '',
+    isbn: '',
+    nbPages: 0,
+    weight: 0,
+    publishingDate: this.datePipe.transform(new Date(), 'yyyy-MM-dd') as string,
+    published: false
+  };
+  submitted = false;
+
+  constructor(private bookService: BookService, private datePipe: DatePipe) {
+  }
 
   ngOnInit(): void {
+  }
+
+  saveTutorial(): void {
+    const data = {...this.book};
+
+    this.bookService.create(data)
+      .subscribe({
+        next: (response) => {
+          console.log(response);
+          this.submitted = true;
+        },
+        error: (err) => {
+          console.log(err);
+        }
+      });
+  }
+
+  newBook(): void {
+    this.submitted = false;
+    this.book = {
+      title: '',
+      description: '',
+      published: false
+    };
   }
 
 }
