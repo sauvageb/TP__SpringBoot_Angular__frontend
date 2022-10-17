cab2c70098c8de39a0d15d6d409e5e52a3ad8f08
sauvageb <sauvageboris.pro@gmail.com>
2022-10-17 17:53:45 +0200

    completing book interface with data received by REST Api

--- src/app/models/book.ts
+++ src/app/models/book.ts
@@ -1,2 +1,11 @@
 export interface Book {
+  id?: number;
+  title?: string;
+  pictureUrl?: string
+  description?: string;
+  isbn?: string;
+  nbPages?: number;
+  weight?: number;
+  published?: boolean;
+  publishingDate?: Date;
 }
