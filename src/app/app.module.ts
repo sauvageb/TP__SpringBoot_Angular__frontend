f2455100315cd80d8657d06a597f2e4be54449c4
sauvageb <sauvageboris.pro@gmail.com>
2022-10-17 12:11:47 +0200

    init project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddBookComponent } from './components/add-book/add-book.component';
+import { BookDetailsComponent } from './components/book-details/book-details.component';
+import { BookListComponent } from './components/book-list/book-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddBookComponent,
+    BookDetailsComponent,
+    BookListComponent
   ],
   imports: [
     BrowserModule,
