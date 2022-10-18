541d2b71089745c700f60526c158c6fb0acdb42d
sauvageb <sauvageboris.pro@gmail.com>
2022-10-18 19:51:12 +0200

    Navbar : link to display login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import {BookListComponent} from "./components/book-list/book-list.component";
 import {BookDetailsComponent} from "./components/book-details/book-details.component";
 import {AddBookComponent} from "./components/add-book/add-book.component";
 import {RegisterComponent} from "./components/register/register.component";
+import {LoginComponent} from "./components/login/login.component";
 
 const routes: Routes = [
   {path: '', redirectTo: 'books', pathMatch: 'full'},
   {path: 'books', component: BookListComponent},
   {path: 'books/:id', component: BookDetailsComponent},
   {path: 'add-book', component: AddBookComponent},
-  {path: 'register', component: RegisterComponent}
+  {path: 'register', component: RegisterComponent},
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
